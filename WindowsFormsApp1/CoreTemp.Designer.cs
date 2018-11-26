d1f1be689008f1273eaa7f2f26a31fe10e4ca84c
Twist <twistedsprites@gmail.com>
2018-11-26 14:54:58 -0600

    Add files via upload

---
+++ WindowsFormsApp1/CoreTemp.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CoreTemp
+{
+    partial class CoreTemp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCPU = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPU Temp";
+            // 
+            // lblCPU
+            // 
+            this.lblCPU.AutoSize = true;
+            this.lblCPU.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPU.Location = new System.Drawing.Point(39, 49);
+            this.lblCPU.Name = "lblCPU";
+            this.lblCPU.Size = new System.Drawing.Size(51, 20);
+            this.lblCPU.TabIndex = 1;
+            this.lblCPU.Text = "label2";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 135);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(165, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(96, 17);
+            this.toolStripStatusLabel1.Text = "Not yet updated.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(165, 157);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lblCPU);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Core Temp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCPU;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ WindowsFormsApp1/CoreTemp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenHardwareMonitor.Hardware;
+using System.Management;
+using System.IO;
+
+namespace CoreTemp
+{
+    public partial class CoreTemp : Form
+    {
+        Timer timer = new Timer();
+        string CPUTemps = string.Empty;
+        public CoreTemp()
+        {
+            InitializeComponent();
+            timer.Tick += Timer_Tick;
+            timer.Interval = 5000; //5 seconds
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            getTemp();
+            toolStripStatusLabel1.Text = "Last updated: " + DateTime.Now.ToLongTimeString();
+
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+
+        }
+        private void getTemp() {
+            var searcher = new ManagementObjectSearcher(@"root\WMI", "SELECT * FROM MSAcpi_ThermalZoneTemperature");
+
+            foreach (ManagementObject service in searcher.Get())
+            {
+                // show the instance
+                var temp = Convert.ToDouble(service.GetPropertyValue("CurrentTemperature"));
+                temp = (temp - 2732) / 10.0;
+                lblCPU.Text = temp.ToString() + "C";
+                var faren = (temp * 9 / 5) +32;
+                lblCPU.Text += " / " + faren + "F";
+
+                CPUTemps+= DateTime.Now +": "+ (temp.ToString() + "C" + " / " + faren + "F" + "\r\n");
+            }
+            File.WriteAllText("CPUTemps.txt",CPUTemps);
+            
+        }
+        
+    }
+}
